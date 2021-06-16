487248021401c9473a70fb6eb2e4bc96d05354cc
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-16 15:03:42 +0100

    🎉 INT COMMIT:

---
+++ index.js
@@ -0,0 +1,36 @@
+// jshint esversion: 10
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// app.use(path.join(express.static(__dirname + '/public')));
+
+app.get('/', (req, res) => {
+    // res.sendFile(__dirname + "/index.html");
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+
+    // res.render(index);
+});
+
+app.get('/contact-me', (req, res) => {
+    // res.render(contact - me);
+    res.sendFile(path.join(__dirname + '/public/contact-me.html'));
+
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/about.html'));
+});
+
+app.use(function(req, res, next) {
+    // res.status(404).render(404);
+    res.status(404).sendFile(path.join(__dirname + '/public/404.html'));
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT${PORT}`);
+});
